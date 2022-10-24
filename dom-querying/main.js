b538192957470d85069ae67ad21ee44fa1da35a3
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-24 16:52:57 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+
+var h1 = document.querySelector('h1');
+console.log(h1);
+
+var explanation = document.querySelector('#explanation');
+console.log(explanation);
+
+var hint = document.querySelector('.hint');
+console.log(hint);
+
+var allPelements = document.querySelectorAll('p');
+console.log(allPelements);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log(exampleLink);
