f0a60f7bd96dba5cd79463809cca907537d2ce52
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-27 23:10:11 +0000

    Completed javascript-view-swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,28 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', matches);
+
+function matches(event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tab.length; i++) {
+      if (event.target === $tab[i]) {
+        event.target.className = 'tab active';
+        var data = event.target.getAttribute('data-view');
+      } else {
+        $tab[i].className = 'tab';
+      }
+    }
+  }
+
+  for (i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') !== data) {
+      $view[i].className = 'hidden';
+
+    } else {
+      $view[i].className = 'tab active';
+    }
+  }
+
+}
