5ed8d5b1a252429be82dffba478e00a68a2d8322
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-27 20:32:55 +0000

    not finished

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,11 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  // var newString = '';
+
+  // var sliceString = string.substr(-firstIndex);
+  // console.log(sliceString);
+  // var firstChar = string.charAt(firstIndex);
+  // var secondChar = string.charAt(secondIndex);
+//  console.log(firstChar, secondChar);
+}
