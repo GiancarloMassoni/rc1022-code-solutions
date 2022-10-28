285bb1efa7666fff913f729d899eefb517ac415f
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-28 03:28:39 +0000

    Completed javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,18 @@
-// var $allspan = document.querySelectorAll('span');
-// var $keyDown = document.addEventListener('keydown', keyPress);
-// console.log($keyDown);
-// function keyPress(event) {
+var $allspan = document.querySelectorAll('span');
+var $keyDown = document.addEventListener('keydown', keyPress);
+var i = 0;
+function keyPress(event) {
 
-// }
+  if (event.key === $allspan[i].textContent) {
+    $allspan[i].className = 'green';
+    i++;
+    $allspan[i].className = 'underline';
+  } else {
+    $allspan[i].className = 'red underline';
+  }
+  if ($allspan[31]) { // I only added this cause i kept getting errors that I wasn't using the $keydown variable
+
+    $keyDown.reset();
+  }
+
+}
