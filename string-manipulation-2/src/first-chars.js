a6addbff010ce71cb116b20568c8a2c9947cea6d
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-31 16:29:30 +0000

    Removed unnecessary steps to firstChar and lastChar

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,8 +1,7 @@
 /* exported firstChars */
 // make a new variable that slices the string at the length argument
-// make a new variable for a new string that concats sliced string and return it
+// make a new variable for a new string that concats sliced string and return it ----this step is not necesarry
 function firstChars(length, string) {
   var slicedString = string.slice(null, length);
-  var finalString = slicedString.concat('');
-  return finalString;
+  return slicedString;
 }
