af2ffaecd7b173bdcea6a90f0d30668d9c6b91cf
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-01 04:14:56 +0000

    pick done

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,12 +1,11 @@
 /* exported pick */
 
 function pick(source, keys) {
-  // var object = Object.entries(source);
   var output = {};
 
-  for (var i = 0; i < source.length; i++) {
-    if (keys === source[keys[i]]) {
-      output += source.i;
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      output[keys[i]] = source[keys[i]];
     }
   }
   return output;
