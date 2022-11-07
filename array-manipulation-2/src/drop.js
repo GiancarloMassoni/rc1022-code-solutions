dc756e68ddabe854f8d9dd4ccd6a0c496b73ce63
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-07 02:29:24 +0000

    Added pseudo code

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,5 +1,7 @@
 /* exported drop */
-
+// create a new array
+// make a loop, looping through the array length
+// if i is less than the count minus one then push that into the new array
 function drop(array, count) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,5 +1,7 @@
 /* exported take */
-
+// create a new array
+// loop through the argument array
+// if i is less than or equal to the count minus one then push that into the new array
 function take(array, count) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
