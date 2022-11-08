0bd7ff14c5aca3caaa3a9e1e0d74874512c3583f
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-08 22:17:10 +0000

    Completed javascript-bank

---
+++ javascript-bank/bank.js
@@ -0,0 +1,43 @@
+/* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && balance === Math.floor(balance)) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    var currentAccountNum = this.nextAccountNumber;
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return currentAccountNum;
+
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+
+  for (var i = 0; i < this.accounts.length; i++) {
+
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+
+  var total = 0;
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    for (var i = 0; i < this.accounts.length; i++) {
+      total += this.accounts[i].getBalance();
+    }
+    return total;
+  }
+};
