2553c00e795595f40fb48f6345d628b0c6616529
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-10 00:57:34 +0000

    not finished

---
+++ javascript-carousel/main.js
@@ -0,0 +1,14 @@
+// var $imgList = document.querySelectorAll('img');
+// var index = 0;
+// var $btnList = document.querySelectorAll('.fa-circle');
+// var $right = document.querySelector('.fa-chevron-right');
+// var $left = document.querySelector('.fa-chevron-left');
+
+// $right.addEventListener('click', nextImg);
+
+// function nextImg(event) {
+//   if (index > $imgList.length) {
+//     index = 0;
+//   }
+
+// }
