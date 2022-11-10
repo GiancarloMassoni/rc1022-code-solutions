456c3279e1eaf4c1a91aba41db57d00a1e3acee5
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-10 22:42:37 +0000

    not finished

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,5 +1,12 @@
 // var $imgList = document.querySelectorAll('img');
 // var index = 0;
+// var images = {
+//   src: ['https://gamertweak.com/wp-content/uploads/2021/12/anti-mage-dota-2-heroes.jpg',
+//     'https://www.dotafire.com/images/hero/icon/alchemist.png',
+//     'https://www.dotafire.com/images/hero/icon/axe.png',
+//     'https://www.dotafire.com/images/hero/icon/faceless-void.png',
+//     'https://www.dotafire.com/images/hero/icon/tiny.png']
+// };
 // var $btnList = document.querySelectorAll('.fa-circle');
 // var $right = document.querySelector('.fa-chevron-right');
 // var $left = document.querySelector('.fa-chevron-left');
