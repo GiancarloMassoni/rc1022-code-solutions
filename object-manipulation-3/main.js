6d80c53834c3e8128bf91f5b2ed8b4f5a0f57f5c
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-28 17:00:44 +0000

    not finished

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,15 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+// function Player(name, hand) {
+//   this.name = name;
+//   this.hand = hand;
+// }
+
+// function Card(rank, suit) {
+//   this.rank = rank;
+//   this.suit = suit;
+// }
+
+// function Deck(card) {
+
+// }
