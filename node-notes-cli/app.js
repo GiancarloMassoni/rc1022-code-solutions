ddeb8b75a328972c6386444b64b2f658fd3430b1
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-12-01 22:56:14 +0000

    Got rid of unnecessary for in loops

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -21,22 +21,16 @@ if (option === 'read') {
 } else if (option === 'update') {
   const updateId = process.argv[3];
   const updatedNote = process.argv[4];
-  for (const keys in notes) {
-    if (keys === updateId) {
-      dataObj.notes[updateId] = updatedNote;
-    }
-  }
+
+  dataObj.notes[updateId] = updatedNote;
+
   const objString = JSON.stringify(dataObj, null, 2);
   fs.writeFile('data.json', objString, err => { if (err) throw err; });
-
 } else if (option === 'delete') {
   const deleteId = process.argv[3];
 
-  for (const keys in notes) {
-    if (keys === deleteId) {
-      delete notes[deleteId];
-    }
-  }
+  delete notes[deleteId];
+
   const objString = JSON.stringify(dataObj, null, 2);
   fs.writeFile('data.json', objString, err => { if (err) throw err; });
 }
