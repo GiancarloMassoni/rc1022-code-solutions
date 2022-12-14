95c0dd4b7ad1beb2b697d5a67de234e5773b87f5
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-12-14 18:51:19 +0000

    Completed react-props-and-expressions

---
+++ react-props-and-expressions/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const CustomButton = props => {
+  return (
+    <button>{props.text}</button>
+  );
+};
+
+const container = document.querySelector('#root');
+
+const root = ReactDOM.createRoot(container);
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+root.render(element);
