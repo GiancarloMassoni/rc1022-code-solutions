d58b338009ac9d757f6f9e1f6a9602081c2029e3
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-12-15 22:38:54 +0000

    for some reason i thought it was only supposed to count up to 5 lmao but i fixed it so it goes infinitely

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -10,27 +10,17 @@ class StopWatch extends React.Component {
 
   handleTimer() {
 
-    if (this.state.time === 5) {
-      this.setState({ clicked: false });
-      this.setState({ time: 0 });
-      clearInterval(this.state.intervalId);
-    } else if (this.state.time === 0) {
-      this.setState({ intervalId: setInterval(this.handleTimer, 1000) });
-      this.setState({ time: this.state.time + 1 });
-    } else if (this.state.time < 5) {
-      this.setState({ time: this.state.time + 1 });
-    }
+    this.setState({ time: this.state.time + 1 });
   }
 
   handleClick() {
-    if (this.state.time === 5 && this.state.clicked === true) {
+    if (this.state.clicked === true) {
       this.setState({ clicked: false });
+      clearInterval(this.state.intervalId);
+
     } else if (this.state.clicked === false && this.state.time === 0) {
       this.setState({ clicked: true });
-      this.handleTimer();
-    } else if (this.state.clicked === true) {
-      clearInterval(this.state.intervalId);
-      this.setState({ clicked: false });
+      this.setState({ intervalId: setInterval(this.handleTimer, 1000) });
     }
 
   }
@@ -48,9 +38,6 @@ class StopWatch extends React.Component {
     if (this.state.clicked === false) {
       playPause = <i className="fa-solid fa-play" onClick={this.handleClick}></i>;
     } else if (this.state.clicked === true) {
-      playPause = <i className="fa-solid fa-pause" onClick={this.handleClick}></i>;
-    }
-    if (time === 5) {
       playPause = <i className="fa-solid fa-pause" onClick={this.handleClick}></i>;
     }
 
