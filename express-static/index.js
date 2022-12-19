76dd5c2d6f1929a541fcce149a36c9c8718aa124
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-12-19 18:00:12 +0000

    Completed express-static

---
+++ express-static/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const directory = path.join(__dirname, '/public');
+const serveFiles = express.static(directory);
+app.use(serveFiles);
+app.listen(3000, () =>
+  console.log('Listening on port 3000'));
+
+app.get('', (req, res) => {
+
+});
