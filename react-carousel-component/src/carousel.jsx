1b690cefb76eff17d9e42f99f598769557ff07d2
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-12-19 19:52:59 +0000

    Completed react-carousel-component

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -7,6 +7,8 @@ class Carousel extends React.Component {
     this.handleImg = this.handleImg.bind(this);
     this.mapDots = this.mapDots.bind(this);
     this.changeSlide = this.changeSlide.bind(this);
+    this.handleRight = this.handleRight.bind(this);
+    this.handleLeft = this.handleLeft.bind(this);
   }
 
   handleImg() {
@@ -27,21 +29,36 @@ class Carousel extends React.Component {
   }
 
   changeSlide(event, index) {
-    // console.log(index);
-    // console.log(this.state);
     clearInterval(this.state.interval);
+    this.setState({ currSlide: index, interval: setInterval(this.handleImg, 3000) });
     if (index) {
-      this.setState({ currSlide: index, interval: setInterval(this.handleImg, 3000) });
       return <i className="fa-solid fa-circle fa-2xl padding" key={index} onClick={event => this.changeSlide(event, index)}></i>;
     } else {
       return <i className="fa-regular fa-circle fa-2xl padding" key={index} onClick={event => this.changeSlide(event, index)}></i>;
     }
   }
 
+  handleRight() {
+    clearInterval(this.state.interval);
+    if (this.state.currSlide === this.props.images.length - 1) {
+      this.setState({ currSlide: 0, interval: setInterval(this.handleImg, 3000) });
+    } else {
+      this.setState({ currSlide: this.state.currSlide + 1, interval: setInterval(this.handleImg, 3000) });
+    }
+  }
+
+  handleLeft() {
+    clearInterval(this.state.interval);
+    if (this.state.currSlide === 0) {
+      this.setState({ currSlide: this.props.images.length - 1, interval: setInterval(this.handleImg, 3000) });
+    } else {
+      this.setState({ currSlide: this.state.currSlide - 1, interval: setInterval(this.handleImg, 3000) });
+    }
+  }
+
   render() {
     const image = this.props.images[this.state.currSlide];
     const dots = this.props.images.map(this.mapDots);
-    // const interval = setInterval(this.props.images.map(this.mapImages), 3000);
 
     if (this.state.image === '') {
       this.setState({ image, currSlide: this.state.currSlide + 1 });
@@ -51,13 +68,13 @@ class Carousel extends React.Component {
       <div className='container'>
         <div className='row align-center justify-center'>
           <div className='col-one-sixth'>
-            <i className="fa-sharp fa-solid fa-chevron-left fa-2xl"></i>
+            <i className="fa-sharp fa-solid fa-chevron-left fa-2xl" onClick={this.handleLeft}></i>
           </div>
           <div className='col-four-sixth'>
             <img src={image}></img>
           </div>
           <div className='col-one-sixth text-right'></div>
-          <i className="fa-sharp fa-solid fa-chevron-right fa-2xl"></i>
+          <i className="fa-sharp fa-solid fa-chevron-right fa-2xl" onClick={this.handleRight}></i>
         </div>
         <div className='row text-center'>
           <div className='col-full'>
